d9813e80ded10c764f60a65c707ac256a5e6fe15
MarinaB <m.beloglazenko@ideas-world.com>
2020-10-30 07:04:15 +0300

    refactor Utils class

--- ege/app/src/main/java/com/deadely/ege/utils/Utils.kt
+++ ege/app/src/main/java/com/deadely/ege/utils/Utils.kt
@@ -6,30 +6,32 @@ import android.util.Base64
 import androidx.core.content.ContextCompat
 import com.deadely.ege.base.App
 
-class Utils {
-    companion object {
+object Utils {
 
-        fun getString(id: Int): String {
-            return App.instance.resources.getString(id)
-        }
-
-        fun getColor(id: Int): Int {
-            return ContextCompat.getColor(App.instance.applicationContext, id)
-        }
-
-        fun decodeString(imageString: String): Bitmap {
-            val decodedString: ByteArray = Base64.decode(imageString, Base64.DEFAULT)
-            return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
-        }
-
-        fun getMiddleValue(list: List<Int?>): Int {
-            var middleCount = 0
-            for (item in list) {
-                item?.let { middleCount += it }
-            }
-            middleCount /= list.size
-            return middleCount
-        }
-
+    @JvmStatic
+    fun getString(id: Int): String {
+        return App.instance.resources.getString(id)
     }
+
+    @JvmStatic
+    fun getColor(id: Int): Int {
+        return ContextCompat.getColor(App.instance.applicationContext, id)
+    }
+
+    @JvmStatic
+    fun decodeString(imageString: String): Bitmap {
+        val decodedString: ByteArray = Base64.decode(imageString, Base64.DEFAULT)
+        return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
+    }
+
+    @JvmStatic
+    fun getMiddleValue(list: List<Int?>): Int {
+        var middleCount = 0
+        for (item in list) {
+            item?.let { middleCount += it }
+        }
+        middleCount /= list.size
+        return middleCount
+    }
+
 }
